83cca76a5b6fb1e0d71f948ce7691c1f1527232d
Mark Dunne <markdunne02@gmail.com>
2020-04-14 11:16:00 +0100

    tidying game

--- public/dotsgame/app.js
+++ public/dotsgame/app.js
@@ -1,12 +1,12 @@
 let GameState = {
-    ChooseLevel: 'ChooseLevel',
-    Playing: 'Playing',
-    GameWon: 'GameWon',
-    GameOver: 'GameOver',
-}
+    ChooseLevel: "ChooseLevel",
+    Playing: "Playing",
+    GameWon: "GameWon",
+    GameOver: "GameOver",
+};
 
 let app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
         GameState,
         gameState: GameState.ChooseLevel,
@@ -17,31 +17,38 @@ let app = new Vue({
     },
     methods: {
         start_level: function (level_idx) {
-            this.currentLevelIndex = level_idx
-            this.currentLevel = this.levels[this.currentLevelIndex]
-            this.game = Game.from_json(this.currentLevel['puzzle'], this)
-            this.gameState = GameState.Playing
+            this.currentLevelIndex = level_idx;
+            this.currentLevel = this.levels[this.currentLevelIndex];
+            this.game = Game.from_json(this.currentLevel["puzzle"], this);
+            this.gameState = GameState.Playing;
         },
         keypress: function (evt) {
             if (this.gameState == GameState.Playing) {
-                this.game.move(evt.key)
+                this.game.move(evt.key);
             }
         },
         onInvalidMove: function (game) {
-            this.gameState = GameState.GameOver
+            this.gameState = GameState.GameOver;
         },
         onWin: function (game) {
-            this.gameState = GameState.GameWon
+            this.gameState = GameState.GameWon;
         },
         draw_game: function () {
             if (this.game != null) {
-                this.game.draw(this.$refs.gameCanvas)
+                this.game.draw(this.$refs.gameCanvas);
             }
         },
     },
     mounted: function () {
-        document.addEventListener('keydown', this.keypress)
-
-        setInterval(this.draw_game, 10)
+        document.addEventListener("keydown", this.keypress);
+        setInterval(this.draw_game, 10);
     },
-})
+
+    updated: function () {
+        let gameCanvas = document.getElementById("gameCanvas");
+        if (gameCanvas !== null) {
+            gameCanvas.width = Math.min(window.innerWidth * 0.9, 1200);
+            gameCanvas.height = gameCanvas.width * 0.66;
+        }
+    },
+});
